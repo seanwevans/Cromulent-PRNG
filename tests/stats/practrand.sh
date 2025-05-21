0703c3bb2fa059b746579278c692ba1f872fc7b0
Sean Evans <sean.w.evans@gmail.com>
2025-05-21 09:49:39 -0400

    added 64TB practrand results: normal

--- tests/stats/practrand.sh
+++ tests/stats/practrand.sh
@@ -1,1 +1,1 @@
-build/dump_raw | practrand/RNG_test stdin64 -tlmin 64TB -a -multithreaded | tee tests/results/practrand64TB.txt
+build/dump_raw | practrand/RNG_test stdin64 -tlmax 128TB -a -multithreaded | tee tests/results/practrand128TB.txt
